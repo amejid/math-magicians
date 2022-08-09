effd958af64b6d1235566a4f8126e0ee933bc734
Abdelmejid Oumer Ali <abdelmejid89@gmail.com>
2022-08-09 15:35:42 +0300

    Setup react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
-import Calculator from './components/Calculator';
+import { Routes, Route } from 'react-router-dom';
 
-const App = () => <Calculator />;
+import Navbar from './components/Navbar';
+import Calculator from './pages/Calculator';
+import Home from './pages/Home';
+import Quote from './pages/Quote';
+
+const App = () => (
+  <>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/calculator" element={<Calculator />} />
+      <Route path="quote" element={<Quote />} />
+    </Routes>
+  </>
+);
 
 export default App;
