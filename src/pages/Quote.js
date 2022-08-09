07e9f68ab52da91a6ce1e5f3ac2957df6ce2f9b8
Abdelmejid Oumer Ali <abdelmejid89@gmail.com>
2022-08-09 18:14:30 +0300

    Create quotes page

---
+++ src/pages/Quote.js
@@ -0,0 +1,13 @@
+const Quote = () => (
+  <section>
+    <p>
+      As far as the laws of mathematics refer to reality, they are not certain, and as far as they
+      are certain, they do not refer to reality.
+    </p>
+    <p>
+      <strong>— Albert Einstein</strong>
+    </p>
+  </section>
+);
+
+export default Quote;
