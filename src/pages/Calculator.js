ace597082dd3fa9e1237b6882abf6a74f447da94
Abdelmejid Oumer Ali <abdelmejid89@gmail.com>
2022-08-09 15:26:40 +0300

    Refactor calculator styles

---
+++ src/pages/Calculator.js
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+import calculate from '../logic/calculate';
+import Button from '../components/Button';
+
+import styles from './Calculator.module.scss';
+
+const Calculator = () => {
+  const [data, setData] = useState({ total: 0, next: null, operation: null });
+
+  const displayHandler = (e) => {
+    const val = e.target.textContent;
+    setData((prevState) => ({ ...prevState, total: val }));
+  };
+
+  const clickHandler = (val) => {
+    const res = calculate(data, val);
+    setData(res);
+  };
+
+  const { total, next, operation } = data;
+  return (
+    <div className={styles.calculator}>
+      <div className={styles.result} onChange={displayHandler}>
+        <p>
+          {total}
+          {operation}
+          {next}
+        </p>
+      </div>
+      <div className={styles.row}>
+        <Button onValueChange={clickHandler} text="AC" />
+        <Button onValueChange={clickHandler} text="+/-" />
+        <Button onValueChange={clickHandler} text="%" />
+        <Button onValueChange={clickHandler} text="÷" extra="btn--op" />
+      </div>
+      <div className={styles.row}>
+        <Button onValueChange={clickHandler} text="7" />
+        <Button onValueChange={clickHandler} text="8" />
+        <Button onValueChange={clickHandler} text="9" />
+        <Button onValueChange={clickHandler} text="x" extra="btn--op" />
+      </div>
+      <div className={styles.row}>
+        <Button onValueChange={clickHandler} text="4" />
+        <Button onValueChange={clickHandler} text="5" />
+        <Button onValueChange={clickHandler} text="6" />
+        <Button onValueChange={clickHandler} text="-" extra="btn--op" />
+      </div>
+      <div className={styles.row}>
+        <Button onValueChange={clickHandler} text="1" />
+        <Button onValueChange={clickHandler} text="2" />
+        <Button onValueChange={clickHandler} text="3" />
+        <Button onValueChange={clickHandler} text="+" extra="btn--op" />
+      </div>
+      <div className={styles.row}>
+        <Button onValueChange={clickHandler} text="0" extra="btn--zero" />
+        <Button onValueChange={clickHandler} text="." />
+        <Button onValueChange={clickHandler} text="=" extra="btn--op" />
+      </div>
+    </div>
+  );
+};
+
+export default Calculator;
